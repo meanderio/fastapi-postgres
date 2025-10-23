e7cec68267c6c9ef52cefeee521050eb5eacb997
Aaron Cahn <acahn22@gmail.com>
2025-10-22 19:42:45 -0500

    create: fastapi end setup to create users and purchases

--- main.py
+++ main.py
@@ -8,14 +8,42 @@ from typing import (
     List,
     Annotated
 )
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
-class ChoiceBase(BaseModel):
-    choice_text: str
-    is_correct: bool
+class UserBase(BaseModel):
+    name: str
 
-class QuestionBase(BaseModel):
-    question_text: str
-    choices: List[ChoiceBase]
+class PurchaseBase(BaseModel):
+    location: str
+    amount: float
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post("/users/")
+async def create_user(user: UserBase, db:db_dependency):
+    db_user = models.Users(
+            name=user.name
+    )
+    db.add(db_user)
+    db.commit()
+
+@app.post("/purchases/")
+async def create_purchase(purchase: PurchaseBase, db: db_dependency):
+    db_purchase = models.Purchases(
+        location=purchase.location,
+        amount=purchase.amount
+    )
+    db.add(db_purchase)
+    db.commit()
--- models.py
+++ models.py
@@ -3,5 +3,20 @@ from sqlalchemy import (
     Column,
     ForeignKey,
     Integer,
-    String
+    String,
+    Float
 )
+from database import Base
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+class Purchases(Base):
+    __tablename__ = 'purchases'
+
+    id = Column(Integer, primary_key=True, index=True)
+    location = Column(String, index=True)
+    amount = Column(Float)
+    purchaser_id = Column(Integer, ForeignKey('users.id'))
