b9e0e8151bb9dc432a2c1927d1abb5d0e3984b8e
Aaron Cahn <acahn22@gmail.com>
2025-11-02 18:11:54 -0600

    update: removing uneeded imports

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
+from typing import Annotated  # List,
+
 from fastapi import (
+    Depends,
     FastAPI,
-    HTTPException,
-    Depends
 )
 from fastapi.middleware.cors import CORSMiddleware
-
 from pydantic import BaseModel
-from typing import (
-    List,
-    Annotated
-)
-import models
-from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 
+import models
+from database import SessionLocal, engine
+
 app = FastAPI(
-    version   = "0.1.0",
-    docs_url  = "/docs",
-    redoc_url = "/redoc",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
 )
 
 app.add_middleware(
@@ -30,13 +27,16 @@ app.add_middleware(
 
 models.Base.metadata.create_all(bind=engine)
 
+
 class UserBase(BaseModel):
     name: str
+
 
 class PurchaseBase(BaseModel):
     location: str
     amount: float
     name: str
+
 
 def get_db():
     db = SessionLocal()
@@ -45,23 +45,22 @@ def get_db():
     finally:
         db.close()
 
+
 db_dependency = Annotated[Session, Depends(get_db)]
 
+
 @app.post("/users/")
-async def create_user(user: UserBase, db:db_dependency):
-    db_user = models.Users(
-            name=user.name
-    )
+async def create_user(user: UserBase, db: db_dependency):
+    db_user = models.Users(name=user.name)
     db.add(db_user)
     db.commit()
+
 
 @app.post("/purchases/")
 async def create_purchase(purchase: PurchaseBase, db: db_dependency):
     user = db.query(models.Users).filter(models.Users.name == purchase.name).first()
     db_purchase = models.Purchases(
-        location=purchase.location,
-        amount=purchase.amount,
-        purchaser_id=user.id
+        location=purchase.location, amount=purchase.amount, purchaser_id=user.id
     )
     db.add(db_purchase)
     db.commit()
